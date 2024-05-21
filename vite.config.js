b209341acc64973879a2c741e1b59a5c6c2283e0
Deepak Kumar <deepaklg02@gmail.com>
2024-05-21 18:05:27 +0530

    MOdified build

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-chartjs-2"],
-    },
-  },
 });
