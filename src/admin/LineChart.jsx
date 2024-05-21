7e26960997cc07b5f013a939525d0d7afd66c002
Deepak Kumar <deepaklg02@gmail.com>
2024-05-21 18:02:09 +0530

    MOdified build

--- src/admin/LineChart.jsx
+++ src/admin/LineChart.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJs } from "chart.js/auto";
 import "./line.css";
 const LineChart = ({ chartData }) => {
   return (
-    <div className = "line">
+    <div className="line">
       <Bar data={chartData} />
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["react-chartjs-2"],
+    },
+  },
 });
