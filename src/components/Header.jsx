8f40e3d753c25d5f4cf903c681bbb62a366ea290
Deepak Kumar <deepaklg02@gmail.com>
2024-05-22 22:59:37 +0530

    making responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="julius flex items-center justify-center bg-white w-screen min-h-fit h-16 ">
-      <h2 className="text-3xl text-center">AcaSync</h2>
+      <Link to="/">
+        <h2 className="text-3xl text-center">AcaSync</h2>
+      </Link>
     </div>
   );
 };
